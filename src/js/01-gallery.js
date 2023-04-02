5912166c236ad7a48d6b21474f95cb1a41b82b5e
Bogdan9623 <bogdankorolishin@gmail.com>
2023-04-02 15:36:04 +0300

    edit task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+galleryList.style.listStyle = "none";
+
+function imageGalleryMarkup(items) {
+    return items
+        .map((item) => `<li class="gallery__item">
+                        <a class="gallery__link" href="${item.original}">
+                            <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+                        </a>
+                    </li>`
+        )
+        .join(" ");
+};
+
+galleryList.insertAdjacentHTML("afterbegin", imageGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionPosition: "bottom", captionDelay: "250" });
